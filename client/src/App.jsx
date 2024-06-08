35f9432ee5ccace91215670021c09ecd3d57e140
David Alexander Averos <david.averos@epn.edu.ec>
2024-06-07 21:10:23 -0500

    refactor: Update Chat component to use className instead of class

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import { Card } from './components/Card';
+import {Chat} from './components/Chat';
 
 export default function App() {
   return (
-    <Card
-      name='Apple Watch'
-      price={500}
-      quantity={10}
+    <Chat 
+      nickname='John Doe'
+      message='Hello, World!'
       photo='https://flowbite.com/docs/images/products/apple-watch.png'
     />
   );
--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,20 +1,16 @@
- const Chat = ({nickname, photo, message}) => {
+ export const Chat = ({nickname, photo, message}) => {
   return (
     
-<div class="flex items-start gap-2.5">
-   <img class="w-8 h-8 rounded-full" src={photo} alt={nickname}/>
-   <div class="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
-      <div class="flex items-center space-x-2 rtl:space-x-reverse">
-         <span class="text-sm font-semibold text-gray-900 dark:text-white">{nickname}</span>
-         <span class="text-sm font-normal text-gray-500 dark:text-gray-400">11:46</span>
+<div className="flex items-start gap-2.5">
+   <img className="w-8 h-8 rounded-full" src={photo} alt={nickname}/>
+   <div className="flex flex-col w-full max-w-[320px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl dark:bg-gray-700">
+      <div className="flex items-center space-x-2 rtl:space-x-reverse">
+         <span className="text-sm font-semibold text-gray-900 dark:text-white">{nickname}</span>
+         <span className="text-sm font-normal text-gray-500 dark:text-gray-400">11:46</span>
       </div>
-      <p class="text-sm font-normal py-2.5 text-gray-900 dark:text-white">{message}</p>
-      <span class="text-sm font-normal text-gray-500 dark:text-gray-400">Delivered</span>
+      <p className="text-sm font-normal py-2.5 text-gray-900 dark:text-white">{message}</p>
+      <span className="text-sm font-normal text-gray-500 dark:text-gray-400">Delivered</span>
    </div>  
 </div>
-
-    
   );
 };
-
-export default Chat;
