244e25d788965ad798f506b15c52b47647ede667
David Alexander Averos <david.averos@epn.edu.ec>
2024-06-14 21:24:25 -0500

    chore: Update dependencies and add Chat component to DashboardRoot.jsx

--- server/index.js
+++ server/index.js
@@ -1,28 +1,35 @@
-const WebSocket = require('ws');
 const http = require('http');
 const express = require('express');
+const routes = require('./routes');
 const path = require('path');
 
 const app = express();
+app.use(express.json());
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+app.use('/api', routes);
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor Express escuchando en el puerto ${PORT}`);
+});
 
-wss.on('connection', (ws) => {
-  ws.on('message', (message) => {
-    console.log(message);
-    wss.clients.forEach((client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
-  });
+const usuarios = [
+  { id: 1, username: 'user1', password: 'pass1' },
+  { id: 2, username: 'user2', password: 'pass2' }
+];
+
+
+// Ruta principal que muestra el dashboard (simulado por ahora)
+app.get('/', (req, res) => {
+  res.send('Dashboard - Bienvenido a mi Ecommerce');
+});
+
+// Ruta de login que solo muestra un formulario simple (simulado)
+app.get('/login', (req, res) => {
+  // Simula el formulario de login
+  res.send('Formulario de login');
 });
 
 app.use(express.static(path.join(__dirname, '../client')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "../client",'index.html'));
-});
-
-server.listen(5000, () => {
-  console.log('Servidor WebSocket en ejecución en el puerto 5173');
 });
