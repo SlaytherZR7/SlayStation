c8ce6cae53fcb55c4044de937695429231f1af99
David Alexander Averos <david.averos@epn.edu.ec>
2024-06-15 12:24:25 -0500

    refactor: Update authentication routes and server configuration

--- server/build/index.js
+++ server/build/index.js
@@ -1,9 +1,9 @@
+// Usa import en lugar de require
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 const app = express();
-
 // __dirname y __filename no están definidos en ESM por defecto
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +11,7 @@ app.use(express.static(path.join(__dirname, '../../client/dist')));
 app.get('/', (_req, res) => {
     res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   });
+// Usa las rutas de autenticación en la aplicación
 app.listen(3000, () => {
     console.log(`Server is running on port 3000. Visit http://localhost:3000/ in your browser.`);
 });
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,21 +3,23 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
-import authRoutes from './routes/authRoutes';
+
+
 const app = express();
 
 // __dirname y __filename no están definidos en ESM por defecto
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-app.use(express.static(path.join(__dirname, '../../../client/dist')));
+
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 app.get('/', (_req, res) => {
-    res.sendFile(path.join(__dirname, '../../../client/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
   });
 
 
   // Usa las rutas de autenticación en la aplicación
-  app.use('/auth', authRoutes);
+
   
   app.listen(3000, () => {
     console.log(`Server is running on port 3000. Visit http://localhost:3000/ in your browser.`);
