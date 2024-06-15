58bda13f2b64f4fbcdb727094ffa4ad4e9a61dfb
JeremyTorres07 <jeremy.torres@epn.edu.ec>
2024-06-14 23:33:56 -0500

    refactor: Update App.jsx to use DashboardRoot component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,19 +9,8 @@ import { ProductView } from './pages/ProductView';
 export default function App() {
   return (
     <>
-    <ProductView className='bg-gray-800 '
-    name="pepe el toro 3D super poderoso pitudo y chingon" 
-    price="12"
-    description={[
-      "Impresora 3D Anycubic Kobra",
-      "PluPlayStation 4 Pro Star Wars Edition - Modelo: 1VSON2156",
-      "Sumérgete en el universo de Star Wars con la PlayStation 4 Pro edición especial!",
-      "Este exclusivo paquete te lleva a las épicas batallas de la galaxia, con un diseño completamente personalizado inspirado en el icónico juegos"
-  ]}
-    quantity="2"
-    state="Nuevo"
-    photo="https://oechsle.vteximg.com.br/arquivos/ids/9396673-1000-1000/image-d33b39767b25457ea62636be88456464.jpg?v=637920459507900000"
-    />
+      <DashboardRoot />
+
     </>
   
   );
