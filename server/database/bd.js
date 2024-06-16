b26d92a78c2359567c12aed596166693ce1bd1a8
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2024-06-16 08:20:27 -0500

    chore: Update API endpoints to use localhost:3000 instead of localhost:1234

--- server/database/bd.js
+++ server/database/bd.js
@@ -1,20 +1,13 @@
 import pkg from 'pg';
 const { Pool } = pkg;
-
-// export const pool = new Pool({
-//   host: process.env.DB_HOST,
-//   port: process.env.DB_PORT,
-//   user: process.env.DB_USER,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-// });
+import 'dotenv/config.js';
 
 export const pool = new Pool({
   host: 'localhost',
   port: 5432,
   user: 'postgres',
   database: 'slaystation',
-  password: '130203',
+  password: '130203db',
 });
 
 pool.on('connect', () => {
