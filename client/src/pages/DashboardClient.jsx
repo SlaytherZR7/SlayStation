4a7a2d508b06098de662ce8fe7ff32a51184f66a
JeremyTorres07 <jeremy.torres@epn.edu.ec>
2024-06-16 12:21:41 -0500

    chore: Update DashboardClient to fetch products from server

--- client/src/pages/DashboardClient.jsx
+++ client/src/pages/DashboardClient.jsx
@@ -115,15 +115,21 @@ const products2 = [
 ];
 
 export const DashboardClient = () => {
+  const [products, setProducts] = useState([]);
+  useState(() => { 
+    fetch('http://localhost:3000/products')
+      .then((response) => response.json())
+      .then((data) => setProducts(data));
+   }, []);
   const [isModalOpen, setIsModalOpen] = useState(false);
   return (
     <div className='bg-gray-900 flex flex-col gap-4'>
       <Navbar photo='src/assets/img-login.jpg' condition='0'/>
-      <Carrusel2 products={products2} />
+      <Carrusel2 products={products} />
       <h1 className='text-white text-2xl font-bold text-center'>Videojuegos</h1>
-      <Carrusel products={products1} />
+      <Carrusel products={products} />
       <h1 className='text-white text-2xl font-bold text-center'>Consolas</h1>
-      <Carrusel products={products1} />
+      <Carrusel products={products} />
       <button
         type='button'
         onClick={() => setIsModalOpen(true)}
